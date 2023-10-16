c0d97d43f8ce1706fa84e9dc58cb366f8abca64a
huth2010 <117372286+huth2010@users.noreply.github.com>
2023-10-16 15:48:24 +0700

    10/16 update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 async function connect(){
 try{
-    mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev')
+    mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected!'));
 }catch(error){
 console.log('false');
